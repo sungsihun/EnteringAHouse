b52556159a9e3aad01efde1f5bd9d01b897b2e4f
SiHun Sung <sungsihun@gmail.com>
2018-08-08 21:45:40 -0400

    Initial Commit

---
+++ EnteringAHouse/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EnteringAHouse
+//
+//  Created by NICE on 2018-08-08.
+//  Copyright © 2018 NICE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ EnteringAHouse/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EnteringAHouse
+//
+//  Created by NICE on 2018-08-08.
+//  Copyright © 2018 NICE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
